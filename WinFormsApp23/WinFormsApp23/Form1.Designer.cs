a0b7376069e462d4b91d9ad1d6d822328324e28e
Oyku Aleyna Gorur <109017470+oykualeyna@users.noreply.github.com>
2025-04-15 06:39:39 +0300

    Add files via upload
    Added files for the maze game project.

---
+++ WinFormsApp23/WinFormsApp23/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace WinFormsApp23
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkMagenta;
+            button1.Location = new Point(4, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(795, 23);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkMagenta;
+            button2.Location = new Point(4, 430);
+            button2.Name = "button2";
+            button2.Size = new Size(795, 23);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.DarkMagenta;
+            button3.Location = new Point(10, 99);
+            button3.Name = "button3";
+            button3.Size = new Size(21, 335);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            button3.MouseHover += button3_MouseHover;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.DarkMagenta;
+            button4.Location = new Point(778, 22);
+            button4.Name = "button4";
+            button4.Size = new Size(21, 312);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            button4.MouseHover += button4_MouseHover;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Plum;
+            button5.Location = new Point(733, 334);
+            button5.Name = "button5";
+            button5.Size = new Size(21, 101);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            button5.MouseHover += button5_MouseHover;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.DarkMagenta;
+            button6.Location = new Point(136, 99);
+            button6.Name = "button6";
+            button6.Size = new Size(21, 252);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            button6.MouseHover += button6_MouseHover;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.DarkMagenta;
+            button7.Location = new Point(270, 99);
+            button7.Name = "button7";
+            button7.Size = new Size(21, 54);
+            button7.TabIndex = 6;
+            button7.UseVisualStyleBackColor = false;
+            button7.MouseHover += button7_MouseHover;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.DarkMagenta;
+            button8.Location = new Point(381, 232);
+            button8.Name = "button8";
+            button8.Size = new Size(21, 202);
+            button8.TabIndex = 7;
+            button8.UseVisualStyleBackColor = false;
+            button8.MouseHover += button8_MouseHover;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.DarkMagenta;
+            button9.Location = new Point(481, 82);
+            button9.Name = "button9";
+            button9.Size = new Size(21, 252);
+            button9.TabIndex = 8;
+            button9.UseVisualStyleBackColor = false;
+            button9.MouseHover += button9_MouseHover;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.DarkMagenta;
+            button10.Location = new Point(581, 22);
+            button10.Name = "button10";
+            button10.Size = new Size(21, 83);
+            button10.TabIndex = 9;
+            button10.UseVisualStyleBackColor = false;
+            button10.MouseHover += button10_MouseHover;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.DarkMagenta;
+            button11.Location = new Point(593, 186);
+            button11.Name = "button11";
+            button11.Size = new Size(21, 252);
+            button11.TabIndex = 10;
+            button11.UseVisualStyleBackColor = false;
+            button11.MouseHover += button11_MouseHover;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.DarkMagenta;
+            button12.Location = new Point(154, 214);
+            button12.Name = "button12";
+            button12.Size = new Size(248, 23);
+            button12.TabIndex = 11;
+            button12.UseVisualStyleBackColor = false;
+            button12.MouseHover += button12_MouseHover;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.DarkMagenta;
+            button13.Location = new Point(353, 82);
+            button13.Name = "button13";
+            button13.Size = new Size(331, 23);
+            button13.TabIndex = 12;
+            button13.UseVisualStyleBackColor = false;
+            button13.MouseHover += button13_MouseHover;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.DarkMagenta;
+            button14.Location = new Point(593, 167);
+            button14.Name = "button14";
+            button14.Size = new Size(91, 23);
+            button14.TabIndex = 13;
+            button14.UseVisualStyleBackColor = false;
+            button14.MouseHover += button14_MouseHover;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.DarkMagenta;
+            button15.Location = new Point(697, 311);
+            button15.Name = "button15";
+            button15.Size = new Size(91, 23);
+            button15.TabIndex = 14;
+            button15.UseVisualStyleBackColor = false;
+            button15.MouseHover += button15_MouseHover;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.DarkMagenta;
+            button16.Location = new Point(154, 328);
+            button16.Name = "button16";
+            button16.Size = new Size(91, 23);
+            button16.TabIndex = 15;
+            button16.UseVisualStyleBackColor = false;
+            button16.MouseHover += button16_MouseHover;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.DarkMagenta;
+            button17.Location = new Point(242, 297);
+            button17.Name = "button17";
+            button17.Size = new Size(21, 54);
+            button17.TabIndex = 16;
+            button17.UseVisualStyleBackColor = false;
+            button17.MouseHover += button17_MouseHover;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.DarkMagenta;
+            button18.Location = new Point(778, 334);
+            button18.Name = "button18";
+            button18.Size = new Size(21, 101);
+            button18.TabIndex = 17;
+            button18.UseVisualStyleBackColor = false;
+            button18.MouseHover += button18_MouseHover;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(760, 337);
+            label1.Name = "label1";
+            label1.Size = new Size(18, 20);
+            label1.TabIndex = 18;
+            label1.Text = "B";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(760, 355);
+            label2.Name = "label2";
+            label2.Size = new Size(17, 20);
+            label2.TabIndex = 19;
+            label2.Text = " i";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(760, 375);
+            label3.Name = "label3";
+            label3.Size = new Size(14, 20);
+            label3.TabIndex = 20;
+            label3.Text = "t";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(759, 395);
+            label4.Name = "label4";
+            label4.Size = new Size(13, 20);
+            label4.TabIndex = 21;
+            label4.Text = "i";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(759, 415);
+            label5.Name = "label5";
+            label5.Size = new Size(15, 20);
+            label5.TabIndex = 22;
+            label5.Text = "ş";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LavenderBlush;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "DOKUNMA";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
---
+++ WinFormsApp23/WinFormsApp23/Form1.cs
@@ -0,0 +1,101 @@
+namespace WinFormsApp23
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yandiniz!");
+        }
+
+        private void button2_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button3_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button4_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button5_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tebrikler, kazandınız...");
+        }
+
+        private void button6_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+
+        private void button7_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button8_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+        private void button9_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button10_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button11_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button12_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button13_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+        private void button14_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button15_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button16_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button17_MouseHover(object sender, EventArgs e)
+        {
+            button1_MouseHover(sender, e);
+        }
+
+        private void button18_MouseHover(object sender, EventArgs e)
+        {
+            MessageBox.Show("Lütfen hile yapmayiniz.");
+        }
+
+
+    }
+}
